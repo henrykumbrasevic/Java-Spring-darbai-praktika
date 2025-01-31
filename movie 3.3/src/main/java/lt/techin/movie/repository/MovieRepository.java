31177d754de20f016620ddde628365ba9d95863e
HenrykUmbrasevic <henryk.umbrasevic@stud.techin.lt>
2025-01-31 15:44:58 +0200

    movie 3.3 task done

---
+++ movie 3.3/src/main/java/lt/techin/movie/repository/MovieRepository.java
@@ -0,0 +1,12 @@
+package lt.techin.movie.repository;
+
+import lt.techin.movie.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+  boolean existsByTitle(String title);
+
+  boolean existsByDirector(String director);
+
+}
